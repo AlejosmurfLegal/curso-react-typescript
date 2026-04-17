353580089289f604fded7ed26982c97bdb3a00c0
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-04-17 16:00:41 -0500

    Clase 11, Creacion de componentes genericos en typeScript

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,19 +1,26 @@
+import { type } from "os";
 import React, { useEffect, useState, useRef } from "react";
+import type { ImgHTMLAttributes } from "react";
 
-type Props = {
-  img: string;
-  alt: string;
+type LazyImageProps = {
+  src: string;
 };
 
-export const RandomFox = ({ img, alt }: Props): React.JSX.Element => {
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+
+type Props = LazyImageProps & ImageNative;
+
+export const LazyImage = ({ src, ...imgProps }: Props): React.JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
-  const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+  const [currentSrc, setCurrentSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=",
+  );
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          setSrc(img)
+          setCurrentSrc(src);
         }
       });
     });
@@ -25,16 +32,7 @@ export const RandomFox = ({ img, alt }: Props): React.JSX.Element => {
     return () => {
       observer.disconnect();
     };
-  }, [img]);
+  }, [src]);
 
-  return (
-    <img
-      ref={node}
-      width={320}
-      height="auto"
-      src={src}
-      alt={alt}
-      className="rounded bg-gray-300"
-    />
-  );
+  return <img ref={node} src={currentSrc} {...imgProps} />;
 };
